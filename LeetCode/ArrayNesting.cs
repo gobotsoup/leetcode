26bc1c7939474b64cb50b72ccde4f0afb9f13a26
gobot <gobot@DESKTOP-SFSNC2E>
2017-05-29 20:56:32 -0700

    Add visited Dict for faster runtime

--- LeetCode/ArrayNesting.cs
+++ LeetCode/ArrayNesting.cs
@@ -12,21 +12,29 @@ namespace LeetCode
         {
             int length = nums.Length;
             int longestSet = 0;
-            
+            var visited = new Dictionary<int, int>();
             for (int setIndex = 0; setIndex < length; setIndex++)
             {
                 HashSet<int> setValues = new HashSet<int>();
                 int current = nums[setIndex];
-                while (setValues.Contains(nums[current]) == false)
+                while (setValues.Contains(nums[current]) == false && !alreadyVisited(visited, 0, current))
                 {
                     setValues.Add(nums[current]);
+                    visited[current] = setValues.Count;
                     current = nums[current];
                 }
 
                 if (setValues.Count > longestSet) longestSet = setValues.Count;
             }
             return longestSet;
+        }
 
+        private bool alreadyVisited(Dictionary<int, int> visited, int currentLength, int value)
+        {
+            if (visited.ContainsKey(value) == false) return false;
+            int visitedLength = visited[value];
+            if (visitedLength > currentLength) return true;
+            return false;
         }
     }
 }
