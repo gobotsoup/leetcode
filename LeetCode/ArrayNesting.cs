d064b62973646eb8394a1b9a65b242ea0587a619
gobot <gobot@DESKTOP-SFSNC2E>
2017-05-29 20:46:00 -0700

    Add ArrayNesting, having timeout errors

---
+++ LeetCode/ArrayNesting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ArrayNestingProblem
+    {
+        public int ArrayNesting(int[] nums)
+        {
+            int length = nums.Length;
+            int longestSet = 0;
+            
+            for (int setIndex = 0; setIndex < length; setIndex++)
+            {
+                HashSet<int> setValues = new HashSet<int>();
+                int current = nums[setIndex];
+                while (setValues.Contains(nums[current]) == false)
+                {
+                    setValues.Add(nums[current]);
+                    current = nums[current];
+                }
+
+                if (setValues.Count > longestSet) longestSet = setValues.Count;
+            }
+            return longestSet;
+
+        }
+    }
+}
