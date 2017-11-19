8c3d403ae5faa86eb921092850640283ba564ee1
gobot <gobot@DESKTOP-SFSNC2E>
2017-11-19 12:05:23 -0800

    Add MaximumSubArray, Fails due to not fast enough, need to optimize

---
+++ LeetCode/MaximumSubArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MaximumSubArray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            int maxFound = nums[0];
+            int endIndxMaxFound = 0;
+            int startIndxMaxFound = 0;
+
+            for (int startIndx = 0; startIndx < nums.Length; startIndx++)
+            {
+                int currSum = 0;
+                for (int endIndx = startIndx; endIndx < nums.Length; endIndx++)
+                {
+                    currSum += nums[endIndx];
+                    if (currSum > maxFound)
+                    {
+                        maxFound = currSum;
+                        endIndxMaxFound = endIndx;
+                        startIndxMaxFound = startIndx;
+                    }
+                }
+            }
+
+            //int[] result = new int[endIndxMaxFound - startIndxMaxFound];
+            //Array.Copy(nums, startIndxMaxFound, result, 0, endIndxMaxFound - startIndxMaxFound);
+            return maxFound;
+        }
+    }
+}
